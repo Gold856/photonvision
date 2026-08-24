18378e627de2a7e104707ba7bd248516d70b4f23
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-08-23 22:13:02 -0400

    test

--- photon-server/src/main/java/org/photonvision/server/Server.java
+++ photon-server/src/main/java/org/photonvision/server/Server.java
@@ -62,7 +62,6 @@ public class Server {
                         javalinConfig -> {
                             javalinConfig.showJavalinBanner = false;
                             javalinConfig.staticFiles.add("web");
-                            javalinConfig.spaRoot.addFile("/", "web/index.html");
                             javalinConfig.registerPlugin(new CorsPlugin(cors -> cors.addRule(CorsRule::anyHost)));
                             javalinConfig.requestLogger.http(
                                     (ctx, ms) -> {
